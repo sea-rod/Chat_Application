62d85d6ebefa0d2a252123d05502fa2f26f483f5
Seamus <Sea11rod2@gmail.com>
2022-07-02 13:19:33 +0530

    initial commit

---
+++ client1.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+
+
+class Client:
+
+    __flag = True
+
+    def __init__(self, host = "127.0.0.1", port = 8080, mess_size = 1024*128, head = 20) -> None:
+        self.host = host
+        self.port = port 
+        self.mess_size = mess_size
+        self.head = head
+    
+
+    def connect(self)->None:
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        code = self.__client.connect_ex((self.host,self.port))
+        if code == 0:
+            print(f"connected to {self.host}:{self.port}")
+            self.t1 = threading.Thread(target = self.receive)
+            self.t1.daemon = True
+            self.t1.start()
+
+
+    def receive(self)->None:
+        while self.__flag:
+            mess = ''
+            raw_mess = self.__client.recv(self.mess_size).decode()
+            mess_len = int(raw_mess[:self.head])
+            mess += str(raw_mess[self.head:])
+            while len(mess) != mess_len:
+                mess += self.__client.recv(self.mess_size).decode()
+            print(mess)
+
+
+    def send(self,mess:str)->bool:
+        mess_head = f"{len(mess):{self.head}}"
+        mess_head += mess
+        self.__client.send(mess_head.encode())
+
+        if mess == "quit":
+            self.__flag = False
+            return False
+
+        return True
+
+
+if __name__ == "__main__":
+    flag = True
+
+    con = Client()
+    con.connect()
+
+    while flag:
+        a = input()
+        flag = con.send(a)
---
+++ server1.py
@@ -0,0 +1,61 @@
+import socket 
+import threading
+
+class Server:
+
+    __client_soc = {}
+
+    def __init__(self, host = "127.0.0.1", port = 8080, mess_size = 1024*128, head = 20) -> None:
+        self.__host = host
+        self.__port = port 
+        self.__mess_size = mess_size
+        self.__head = head
+    
+    def listen(self):
+        self.__server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.__server.bind((self.__host,self.__port))
+        print("sever listening...")
+        self.__server.listen(5)
+    
+    def accept(self):
+        while True:
+            conn ,addr = self.__server.accept()
+            print(f"{addr} connected....")
+            conn.send(("                  16Enter your name:").encode())
+            name = conn.recv(self.__mess_size).decode()
+            self.__client_soc[conn] = name[self.__head:]
+            conn.send(("                  39Type your message or type quit to exit:").encode())
+            t1=threading.Thread(target = self.receive,args=(conn,))
+            t1.start()
+
+    def receive(self, conn:socket):
+        while True:
+            mess = ''
+            raw_mess = conn.recv(self.__mess_size).decode()
+            mess_len = int(raw_mess[:self.__head])
+            mess += raw_mess[self.__head:]
+
+            while len(mess) < mess_len:
+                mess += conn.recv(self.__mess_size).decode()
+
+            cl_mess = f'[+][{self.__client_soc[conn]}]:{mess}'
+            mess_len = (f'{len(cl_mess):{self.__head}}')
+            mess_len += cl_mess
+
+            self.__send(mess_len)
+
+            if mess == "quit":
+                del self.__client_soc[conn]
+                break
+    
+
+    def __send(self,mess:str):
+        for soc in self.__client_soc.keys():
+            soc.send(mess.encode())
+        print(mess)
+
+
+if __name__ == "__main__":
+    a = Server()
+    a.listen()
+    a.accept()
