1e39bdc607ae2afa3de0d8fdc47163a11a886980
Seamus <Sea11rod2@gmail.com>
2022-08-23 19:16:49 +0530

    update main.py

--- server folder/main.py
+++ server folder/main.py
@@ -1,5 +1,24 @@
 from server import Server
+import socket
 
-srv = Server()
-srv.listen()
-srv.accept()
+
+try:
+    ip = input("Enter the host name or ip address you to bind the server with:")
+    port = int(input("Enter the port:"))
+    ip = socket.gethostbyname(ip)
+    srv = Server(ip,port)
+    srv.listen()
+    srv.accept()
+
+
+except socket.gaierror:
+    print("Please a enter a valid ip or host name and try again")
+
+    
+except ValueError:
+    print("Port number should be a integer")
+
+
+
+
+
