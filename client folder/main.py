4c275a651131f3f7ad2b96e68eed0251a7d53b17
Seamus <Sea11rod2@gmail.com>
2022-08-23 19:09:56 +0530

    update main.py

--- client folder/main.py
+++ client folder/main.py
@@ -1,11 +1,23 @@
 from client import Client
+import socket
 
-clt = Client()
-flag = clt.connect()
 
-while flag:
-    mess = input()
-    flag = clt.send(mess)
 
-if not flag:
-    print("Connection failed retry!!")
+
+try:
+    ip = input("Enter the host name or ip address:")
+    port = int(input("Enter the port:"))
+    ip = socket.gethostbyname(ip)
+    clt = Client(ip,port)
+    flag = clt.connect()
+    while flag:
+        mess = input()
+        flag = clt.send(mess)
+
+except socket.gaierror:
+    print("Please a enter a valid ip or host name and try again")
+
+    
+except ValueError:
+    print("Port number should be a integer")
+
